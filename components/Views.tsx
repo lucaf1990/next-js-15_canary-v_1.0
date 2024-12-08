7e9d1b0a89a8ed8e25399c3df5c153ff78452c2c
Luca Forma <luca.forma@sparkfabrik.com>
2024-12-08 02:40:58 +0100

    unstableAfter -> after

--- components/Views.tsx
+++ components/Views.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { client } from "@/sanity/lib/client";
 import { VIEWS_QUERY_BY_ID } from "@/lib/query";
 import { writeClient } from "@/sanity/lib/write";
-import { unstable_after as after } from "next/server";
+import { after } from "next/server";
 const Views = async ({ id }: { id: string }) => {
   const totalViews = (await client
     .withConfig({ useCdn: false })
